6ee7bba0a1465f9a6beeb3394543bd523a6f92fa
maxi5775 <maxicastillo1mediob@gmail.com>
2026-07-03 10:00:39 -0400

    siguiendo la clase Nodo.h creo la clase Nodo.cpp

---
+++ data_structures/Nodo.cpp
@@ -0,0 +1,29 @@
+#include "Nodo.h"
+#include "../classes/Cancion.h"
+#include <cstring>
+#include <iostream>
+
+NodoCancion::NodoCancion(Cancion* d) : dato(d), siguiente(nullptr), anterior(nullptr) {}
+
+NodoCancion::~NodoCancion() {}
+
+NodoCadena::NodoCadena(const char* d) : siguiente(nullptr), anterior(nullptr) {
+    dato = new char[strlen(d) + 1];
+    strcpy(dato, d);
+}
+
+NodoCadena::~NodoCadena() {
+    if (dato) delete[] dato;
+}
+
+NodoArbolCancion::NodoArbolCancion(Cancion* d) : dato(d), izquierda(nullptr), derecha(nullptr), altura(1) {}
+
+NodoArbolCancion::~NodoArbolCancion() {}
+
+NodoTrie::NodoTrie() : esFinPalabra(false), frecuencia(0) {
+    for (int i = 0; i < 36; i++) {
+        hijos[i] = nullptr;
+    }
+}
+
+NodoTrie::~NodoTrie() {}
