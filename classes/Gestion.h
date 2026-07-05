5983c35bd57cfa1b7706a52b0168436424bc1ec6
maxi5775 <maxicastillo1mediob@gmail.com>
2026-07-05 10:48:03 -0400

    agrego Gestion.h

---
+++ classes/Gestion.h
@@ -0,0 +1,37 @@
+#ifndef GESTION_H
+#define GESTION_H
+
+#include "../data_structures/ListaCancion.h"
+#include "../data_structures/AVLCancion.h"
+#include "../data_structures/Trie.h"
+#include "../data_structures/HeapCancion.h"
+#include "Cancion.h"
+#include "Busqueda.h"
+#include "Ranking.h"
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cctype>
+
+class Gestion {
+private:
+    ListaCancion* canciones;
+    AVLCancion* cancionesPorArtista;
+    Busqueda* busqueda;
+    Ranking* ranking;
+    int proximoIdCancion;
+
+public:
+    Gestion();
+    ~Gestion();
+
+    void cargarCancionesDesdeArchivo(const char* archivo);
+    void guardarCancionesEnArchivo(const char* archivo);
+    void mostrarTodasLasCanciones();
+    void reproducirCancion(int idCancion);
+    void buscarCancion(const char* prefijo);
+    void mostrarTop10();
+    ListaCancion* getCanciones();
+};
+
+#endif // GESTION_H
