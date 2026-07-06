404b0177b82d674e152f505c0a245143e22795c6
DamianVR <damian.valdivia@alumnos.ucn.cl>
2026-07-05 22:30:36 -0400

    mostrar top 10

--- classes/Ranking.cpp
+++ classes/Ranking.cpp
@@ -1,4 +1,8 @@
 #include "Ranking.h"
+#include <iostream>
+#include <cstring>
+
+using namespace std;
 
 Ranking::Ranking() {
     heapCanciones = new HeapCancion(100);
@@ -39,25 +43,35 @@ void Ranking::obtenerTop10Artistas(ListaCancion& resultados) {
 }
 
 void Ranking::mostrarTop10Canciones() {
-    std::cout << "\n=== TOP 10 CANCIONES ===" << std::endl;
+    cout << "Ranking TOP 10 Canciones mas escuchadas:\n" << endl;
     Cancion** elementos = heapCanciones->obtenerElementos();
     
     int cantidad = 0;
     while (cantidad < 10 && cantidad < heapCanciones->obtenerTamanio()) {
-        std::cout << cantidad + 1 << ". ";
-        elementos[cantidad]->mostrar();
+        cout << " " << cantidad + 1 << ". [" 
+             << elementos[cantidad]->getReproducciones() << "] " 
+             << elementos[cantidad]->getNombre() << " - " 
+             << elementos[cantidad]->getArtista() << endl;
         cantidad++;
     }
+    
+    cout << "\nOpciones:" << endl;
+    cout << "R<num> - Reproducir cancion seleccionada" << endl;
+    cout << "A<num> - Agregar cancion seleccionada al final de la lista de reproduccion actual" << endl;
+    cout << "A - Top 10 artistas mas escuchados" << endl;
+    cout << "V - Volver al menu principal" << endl;
 }
 
 void Ranking::mostrarTop10Artistas() {
-    std::cout << "\n=== TOP 10 ARTISTAS ===" << std::endl;
+    cout << "Ranking TOP 10 Artistas mas escuchados:\n" << endl;
     Cancion** elementos = heapArtistas->obtenerElementos();
     
     int cantidad = 0;
     while (cantidad < 10 && cantidad < heapArtistas->obtenerTamanio()) {
-        std::cout << cantidad + 1 << ". " << elementos[cantidad]->getNombre() 
-                  << " (" << elementos[cantidad]->getReproducciones() << " reproducciones)" << std::endl;
+        cout << " " << cantidad + 1 << ". " << elementos[cantidad]->getArtista() << endl;
         cantidad++;
     }
+    
+    cout << "\nOpciones:" << endl;
+    cout << "Seleccione el numero de artista para ver sus canciones o X para salir." << endl;
 }
